26b38a55ad3ff1b66b8f7a08a0ede7d4eceaa51a
Marius Schneider <ms@schneiderbad.de>
2026-04-09 12:10:49 +0200

    add output equation to the model

--- models/nonlinear_model.m
+++ models/nonlinear_model.m
@@ -1,15 +1,16 @@
-function dy = nonlinear_model(y, u, p)
+function [dx, y] = nonlinear_model(x, u, p)
 %NONLINEAR_MODEL implements the nonlinear system equation
-%   y: state vector [h1; h2]
+%   x: state vector [h1; h2]
 %   u: input vector [v; d]
 %   p: parameters
 
-    v1 = sqrt(2*p.g*y(1));
-    v2 = sqrt(2*p.g*y(2));
+    v1 = sqrt(2*p.g*x(1));
+    v2 = sqrt(2*p.g*x(2));
 
-    dy1 = 1/p.A1 * (u(1) - p.a1*p.mu*v1 - u(2));
-    dy2 = 1/p.A2 * (p.a1*p.mu*v1 - p.a2*p.mu*v2);
+    dx1 = 1/p.A1 * (u(1) - p.a1*p.mu*v1 - u(2));
+    dx2 = 1/p.A2 * (p.a1*p.mu*v1 - p.a2*p.mu*v2);
 
-    dy = [dy1; dy2];
+    dx = [dx1; dx2];
+    y = x(2);
 end
 
