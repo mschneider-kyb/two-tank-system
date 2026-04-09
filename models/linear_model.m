c83ee8dfb274b4a4580b9ab8a27d56173686f4e9
Marius Schneider <ms@schneiderbad.de>
2026-04-09 11:51:15 +0200

    remove obsolet linear model function

--- models/linear_model.m
+++
@@ -1,8 +0,0 @@
-function [dx, y] = linear_model(x, u, d, sys)
-%LINEAR_MODEL Summary of this function goes here
-%   Detailed explanation goes here
-
-    dx = sys.A * x + sys.B(1,:) * u + sys.B(2,:) * d;
-    y = sys.C * x;
-end
-
