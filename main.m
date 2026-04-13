c4e8a07feea275b6e9c3ca66219b23f30e995c3e
Marius Schneider <ms@schneiderbad.de>
2026-04-13 20:19:18 +0200

    fix problem in the computation of G2 for cascade control

--- main.m
+++ main.m
@@ -22,8 +22,8 @@ grid on;
 %% Output control using loopshaping
 
 % get controller
-Kp = 0.0009;
-Ki = 0.00005;
+Kp = 0.00067;
+Ki = 0.0000045;
 K = pid(Kp,Ki);
 
 % Loop shaping is performed on the open-loop transfer function L(s) = K(s) * G_yv(s).
@@ -31,7 +31,7 @@ K = pid(Kp,Ki);
 % the characteristic equation (1 + L(s) = 0) depends only on the feedback 
 % path via the control input 'v'.
 L = G*K;
-margin(L(1),{0.00001,10});
+plot_bode(L(1), {0.00001,10}, "Bode Plot of the Open-Loop Tank System", "bode_plot_pi_controller");
 grid on;
 
 % simulate
@@ -48,30 +48,34 @@ sys_new = ss(sys.A, sys.B, C_new, D_new);
 G_new = tf(sys_new);
 
 % choose controller
-Kp1 = 0.12;
-Ki1 = 0.0;
+Kp1 = 6.5; 
+Ki1 = 0.01;
 K1 = pid(Kp1,Ki1);
 
-Kp2 = 3.5;
-Ki2 = 0.05;
+Kp2 = 1.0;
+Ki2 = 0.08;
 K2 = pid(Kp2,Ki2);
 
 % loop shape inner feedback loop
-L_in = G_new(1,1)*K1;
+G1 = G_new(1,1);
+L_in = G1 * p.v_max * K1;
 
 figure(1);
-margin(L_in,{1,1000});
+margin(L_in);
 grid on;
 
 % loop shape outer feedback loop
-sys_inner_cl = feedback(K1 * G_new(1,1), 1);
-G_outer = sys_inner_cl * G_new(2,1);
-L_out = G_outer*K2;
+sys_inner_cl = feedback(L_in, 1, -1);
+G2 = G_new(2,1) / G_new(1,1); % tf from h1 to h2
+G_outer = 1/p.h_max * G2 * sys_inner_cl;
+L_out = G_outer * K2;
+
+L_out2 = 1/p.h_max * ((G2*L_in*K2)/(1+L_in));
 
 figure(2);
-margin(L_out,{0.1,100});
+margin(L_out);
 grid on;
 
 % simulate
-out_casc = sim("sim_cascade_model", "StopTime", "1000");
-plot_results(out_casc);
+% out_casc = sim("sim_cascade_model", "StopTime", "1000");
+% plot_results(out_casc);
